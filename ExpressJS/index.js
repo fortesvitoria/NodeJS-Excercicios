d12689def3aebf9cbccc93ee0b93cd2384abe32d
Vitoria Fortes <fortesvitoria@gmail.com>
2024-03-13 16:51:38 -0300

    comita aula 203

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,9 +1,20 @@
 // Server
-
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Helo, I'm Vito!</h1>")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Helo, I'm Vito!</h1> <h2>You're at the contact page!</h2>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Helo, I'm Vito!</h1> <h2>You're at the about page!</h2>")
+})
 
 app.listen(port, () => {
 console.log(`Server running on port ${port}.`)
